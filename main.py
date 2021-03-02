8bdec6b0843e828cfcd856884e12ddd635ff37af
isFakeAccount <trevorphillips@gmx.us>
2021-03-02 14:38:27 -0500

    Feature adding and few bug fixed
    [Added] The bot can now reply your own profile or someone else's using profile command
    [Added] The bot removes everything from users that are not registered
    [Fixed] The bug where trello card would return members

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import traceback
 import praw
 import prawcore
 import requests
-import trello
 from dotenv import load_dotenv
 from trello import TrelloClient
 
@@ -71,30 +70,30 @@ def add_emoji(redditor, user_flair, flair_template_id, emoji):
     return user_flair
 
 
-def set_platform_flair(submission, json_data):
-    user_flair = submission.author_flair_text
+def set_platform_flair(comment_or_submission, json_data):
+    user_flair = comment_or_submission.author_flair_text
     regex = re.compile('xbox|playstation|pc', re.IGNORECASE)
     if user_flair is not None and user_flair != '':
         match = re.search(regex, str(user_flair))
         if match:
             return None
         else:
-            flair_template_id = submission.author_flair_template_id
+            flair_template_id = comment_or_submission.author_flair_template_id
             if 'XBL' in json_data.keys():
-                user_flair = add_emoji(submission.author, user_flair, flair_template_id, ':xbox:')
+                user_flair = add_emoji(comment_or_submission.author, user_flair, flair_template_id, ':xbox:')
             if 'PSN' in json_data.keys():
-                user_flair = add_emoji(submission.author, user_flair, flair_template_id, ':playstation:')
+                user_flair = add_emoji(comment_or_submission.author, user_flair, flair_template_id, ':playstation:')
             if 'PC' in json_data.keys():
-                add_emoji(submission.author, user_flair, flair_template_id, ':pc:')
+                add_emoji(comment_or_submission.author, user_flair, flair_template_id, ':pc:')
     else:
         user_flair = 'Karma: 0'
         flair_template_id = '3c680234-4a4d-11eb-8124-0edd2b620987'
         if 'XBL' in json_data.keys():
-            user_flair = add_emoji(submission.author, user_flair, flair_template_id, ':xbox:')
+            user_flair = add_emoji(comment_or_submission.author, user_flair, flair_template_id, ':xbox:')
         if 'PSN' in json_data.keys():
-            user_flair = add_emoji(submission.author, user_flair, flair_template_id, ':playstation:')
+            user_flair = add_emoji(comment_or_submission.author, user_flair, flair_template_id, ':playstation:')
         if 'PC' in json_data.keys():
-            add_emoji(submission.author, user_flair, flair_template_id, ':pc:')
+            add_emoji(comment_or_submission.author, user_flair, flair_template_id, ':pc:')
     return None
 
 
@@ -121,15 +120,11 @@ def send_message_to_discord(message_param):
 # Removes the archived cards from list
 def delete_archived_cards_and_check_desc(search_result, search_query):
     for card in search_result:
-        # Some search query returns the boards and the members which creates issue later
-        if not isinstance(card, trello.Card):
-            search_result.remove(card)
-            continue
-        # closed means the card is archived
+        # If Card is archived remove it
         if card.closed:
             search_result.remove(card)
         # Double check to make sure that search query is in card description
-        if search_query.lower() not in card.description.lower().replace('\\', ''):
+        elif search_query.lower() not in card.description.lower().replace('\\', ''):
             search_result.remove(card)
     return search_result
 
@@ -137,22 +132,119 @@ def delete_archived_cards_and_check_desc(search_result, search_query):
 # Searches in trello board using trello api and return the search result in a list\
 # The list is empty if there are no search results
 def search_in_boards(search_query):
-    try:
-        # escapes the special characters so the search result is exact not from wildcard (e.g '-')
-        search_result = trello_client.search(query=re.escape(search_query), cards_limit=1)
-        search_result_escaped_underscore = list()
-        # If underscore is in search query, we need to search it escaped and non escaped
-        if "_" in search_query:
-            search_result_escaped_underscore = trello_client.search(
-                query=re.escape(search_query.replace("_", "\\_")), cards_limit=1)
-        # Adding results from both searches
-        search_result = search_result + search_result_escaped_underscore
-        # Removing duplicate search results
-        search_result = list(set(search_result))
-        search_result = delete_archived_cards_and_check_desc(search_result, search_query)
-    except NotImplementedError:
-        raise NotImplementedError(search_query)
+    # escapes the special characters so the search result is exact not from wildcard (e.g '-')
+    search_result = trello_client.search(query=re.escape(search_query), cards_limit=1, models=['cards'])
+    search_result_escaped_underscore = list()
+    # If underscore is in search query, we need to search it escaped and non escaped
+    if "_" in search_query:
+        search_result_escaped_underscore = trello_client.search(
+            query=re.escape(search_query.replace("_", "\\_")), cards_limit=1, models=['cards'])
+    # Adding results from both searches
+    search_result = search_result + search_result_escaped_underscore
+    # Removing duplicate search results
+    search_result = list(set(search_result))
+    search_result = delete_archived_cards_and_check_desc(search_result, search_query)
     return search_result
+
+
+def comment_user_profile_on_submission(redditor, submission, trello_card):
+    table = [
+        '|**Reddit username**|**Account Creation Date**|**Email Verified**|**Reddit Karma**|',
+        '|:-|:-|:-|:-|', '|u/{}|{}|{}|{}|', '|**{}**|**XBL**|**PSN**|**PC**|', '|{}|{}|{}|{}|',
+        '\n**Note: If the the following user is trading with GamerTag that is not listed here. '
+        'Please report it to moderators immediately. To get all the bot commands summary just comment'
+        ' `!bot commands`**'
+    ]
+    # Assume that trading karma is 0
+    trading_karma = 0
+    # If user has a flair we update the karma value otherwise it remains 0
+    user_flair = submission.author_flair_text
+    if user_flair is not None and user_flair != '':
+        user_flair_split = user_flair.split()
+        trading_karma = user_flair_split[-1]
+        # For regular users the table says karma otherwise it will say courier, or bot etc...
+        if 'Karma' in user_flair_split[-2]:
+            table[3] = table[3].format('Trading Karma')
+        else:
+            table[3] = table[3].format(user_flair_split[-2].replace(':', ''))
+    else:
+        table[3] = table[3].format('Trading Karma')
+
+    if trello_card[0].board == user_database:
+        # Get submission author information
+        reddit_karma = redditor.comment_karma + redditor.link_karma
+        # converting karma to readable number e,g 10,000 to 10k
+        reddit_karma = readable_number(reddit_karma)
+        account_created = redditor.created_utc
+        # Get when account was created
+        date_created = datetime.datetime.fromtimestamp(account_created)
+        # get how long ago was the account created
+        account_age = account_age_readable_form(account_created)
+        # formatting data in nice string
+        date = '{} - {}'.format(f'{date_created:%D}', account_age)
+        table[2] = table[2].format(redditor.name, date, redditor.has_verified_email, reddit_karma)
+
+        # Getting data from trello card description
+        json_data = json.loads(trello_card[0].description)
+        set_platform_flair(submission, json_data)
+        xbl = 'N/A'
+        psn = 'N/A'
+        pc = 'N/A'
+
+        # Putting the values in table based off the data from description
+        for key, value in json_data.items():
+            if key == 'XBL':
+                xbl = value[0]
+            elif key == 'PSN':
+                psn = value[0]
+            elif key == 'PC':
+                pc = value[0]
+        table[4] = table[4].format(trading_karma, xbl, psn, pc)
+        comment_body = '\n'.join(table)
+        reply(submission, comment_body)
+
+
+def comment_user_profile(redditor, comment, trello_card):
+    table = [
+        '|**Platform**|**Username**|', '|:-|:-|', '|Reddit|u/{}|', '|XBL|{}|', '|PSN|{}|', '|PC|{}|',
+        '\n^(Note: If the the following user is trading with GamerTag that is not listed here. Please report it '
+        'to moderators immediately.)'
+    ]
+    if len(trello_card) > 0 and trello_card[0].board == user_database:
+        # Getting data from trello card description
+        json_data = json.loads(trello_card[0].description)
+        xbl = 'N/A'
+        psn = 'N/A'
+        pc = 'N/A'
+        # Putting the values in table based off the data from description
+        for key, value in json_data.items():
+            if key == 'XBL':
+                xbl = value[0]
+            elif key == 'PSN':
+                psn = value[0]
+            elif key == 'PC':
+                pc = value[0]
+        table[2] = table[2].format(redditor.name)
+        table[3] = table[3].format(xbl)
+        table[4] = table[4].format(psn)
+        table[5] = table[5].format(pc)
+        comment_body = '\n'.join(table)
+        reply(comment, comment_body)
+    else:
+        reply(comment, "u/{} has not registered their GamerTag with us. Please take precaution when trading with them."
+              .format(redditor.name))
+
+
+# Remove all the stuff posted by unregistered users
+def remove_content_from_unregistered_user(comment_or_submission):
+    comment_or_submission.mod.remove(mod_note='User not registered')
+    message_body = "Hi u/{}! It seems that you have not registered your IGN/Gamertag in our system. In order to keep " \
+                   "you and the community safe. We decided to make the registration compulsory if you want to trade " \
+                   "here. It only take a couple of minutes to register. All you need to do is send me a " \
+                   "[chat message](https://www.reddit.com/chat/). I shall provide you the instructions from that " \
+                   "point on and within a matter of minutes. You will be able to trade on this subreddit. Thank you " \
+                   "for your corporation!".format(comment_or_submission.author.name)
+    comment_or_submission.author.message('Please register your GT!', message_body)
 
 
 if __name__ == '__main__':
@@ -160,81 +252,43 @@ if __name__ == '__main__':
     blacklist_board = boards[0]
     user_database = boards[1]
 
-    submission_stream = fallout76marketplace.stream.submissions(skip_existing=True)
+    # Gets 100 historical comments
+    comment_stream = fallout76marketplace.stream.comments(pause_after=-1, skip_existing=True)
+    # Gets 100 historical submission
+    submission_stream = fallout76marketplace.stream.submissions(pause_after=-1, skip_existing=True)
     failed_attempt = 1
 
     print("Bot is now live!", time.strftime('%I:%M %p %Z'))
     while True:
         try:
+            # Gets comments and if it receives None, it switches to posts
+            for comment in comment_stream:
+                if comment is None or comment.author.name == "AutoModerator":
+                    break
+                result = search_in_boards(comment.author.name)
+                if len(result) <= 0:
+                    remove_content_from_unregistered_user(comment)
+                else:
+                    match = re.match(r"^(!PROFILE|PROFILE!)", comment.body.strip(), re.IGNORECASE)
+                    if match:
+                        search = match = re.search(r"u/[^\s\]]+", comment.body.strip(), re.IGNORECASE)
+                        if search:
+                            result = search_in_boards(search.group().replace('u/', ''))
+                            comment_user_profile(reddit.redditor(search.group().replace('u/', '')), comment, result)
+                        else:
+                            comment_user_profile(comment.author, comment, result)
+
+            # Gets posts and if it receives None, it switches to comments
             for submission in submission_stream:
-                if not flair_checks(submission):
-                    continue
-                table = ['|**Reddit username**|**Account Creation Date**|**Email Verified**|**Reddit Karma**|',
-                         '|:-|:-|:-|:-|', '|u/{}|{}|{}|{}|', '|**{}**|**XBL**|**PSN**|**PC**|', '|{}|{}|{}|{}|',
-                         '\n**Note: If the the following user is trading with GamerTag that is not listed here. '
-                         'Please report it to moderators immediately. To get all the bot commands summary just comment'
-                         ' `!bot commands`**']
-                # Assume that trading karma is 0
-                trading_karma = 0
-                # If user has a flair we update the karma value otherwise it remains 0
-                user_flair = submission.author_flair_text
-                if user_flair is not None and user_flair != '':
-                    user_flair_split = user_flair.split()
-                    trading_karma = user_flair_split[-1]
-                    # For regular users the table says karma otherwise it will say courier, or bot etc...
-                    if 'Karma' in user_flair_split[-2]:
-                        table[3] = table[3].format('Trading Karma')
-                    else:
-                        table[3] = table[3].format(user_flair_split[-2].replace(':', ''))
-                else:
-                    table[3] = table[3].format('Trading Karma')
-
-                # Check if the user is registered
+                if submission is None:
+                    break
                 result = search_in_boards(submission.author.name)
-                if len(result) > 0:
-                    if result[0].board == user_database:
-                        # Get submission author information
-                        author = submission.author
-                        reddit_karma = author.comment_karma + author.link_karma
-                        # converting karma to readable number e,g 10,000 to 10k
-                        reddit_karma = readable_number(reddit_karma)
-                        account_created = author.created_utc
-                        # Get when account was created
-                        date_created = datetime.datetime.fromtimestamp(account_created)
-                        # get how long ago was the account created
-                        account_age = account_age_readable_form(account_created)
-                        # formatting data in nice string
-                        date = '{} - {}'.format(f'{date_created:%D}', account_age)
-                        table[2] = table[2].format(author.name, date, author.has_verified_email, reddit_karma)
-
-                        # Getting data from trello card description
-                        json_data = json.loads(result[0].description)
-                        set_platform_flair(submission, json_data)
-                        xbl = 'N/A'
-                        psn = 'N/A'
-                        pc = 'N/A'
-
-                        # Putting the values in table based off the data from description
-                        for key, value in json_data.items():
-                            if key == 'XBL':
-                                xbl = value[0]
-                            elif key == 'PSN':
-                                psn = value[0]
-                            elif key == 'PC':
-                                pc = value[0]
-                        table[4] = table[4].format(trading_karma, xbl, psn, pc)
-                        comment_body = '\n'.join(table)
-                        reply(submission, comment_body)
+                if len(result) <= 0:
+                    remove_content_from_unregistered_user(submission)
                 else:
-                    submission.mod.remove(mod_note='User not registered')
-                    comment_body = "Hi u/{}! It seems that you have not registered your IGN/Gamertag in our system. " \
-                                   "In order to keep you and the community safe. We decided to make the registration " \
-                                   "compulsory if you want to trade here. It only take a couple of minutes to " \
-                                   "register. All you need to do is send me a chat message. I shall provide you the " \
-                                   "instructions from that point on and within a matter of minutes. You will be able " \
-                                   "to trade on this subreddit. Thank you for your corporation!".format(
-                                    submission.author.name)
-                    reply(submission, comment_body)
+                    if flair_checks(submission):
+                        comment_user_profile_on_submission(submission.author, submission, result)
+
         except Exception as stream_exception:
             tb = traceback.format_exc()
             try:
@@ -250,4 +304,5 @@ if __name__ == '__main__':
                 # Try again after a pause
                 time.sleep(120 * failed_attempt)
                 failed_attempt = failed_attempt + 1
-            submission_stream = fallout76marketplace.stream.submissions(skip_existing=True)
+            comment_stream = fallout76marketplace.stream.comments(pause_after=-1, skip_existing=True)
+            submission_stream = fallout76marketplace.stream.submissions(pause_after=-1, skip_existing=True)
